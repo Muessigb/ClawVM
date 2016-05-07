a558e3e38406e2a1fd6abde66cf46d0c57f2d0c3
Benedikt Müssig <benedikt.muessig@gmail.com>
2016-05-07 03:28:08 +0200

    Improvement by MateoConLechuga

--- main.c
+++ main.c
@@ -4,8 +4,10 @@
 // PC main
 int main(int argc, char **argv)
 {
-    
-    
+    claw_pool_vcreate(25);
+    claw_pvar* my_arr;
+    my_arr = claw_pool_vget_a(0);
+    printf("%d", my_arr->size);
     
 	return 0;
 }
--- vars.c
+++ vars.c
@@ -9,31 +9,23 @@ uint16_t claw_var_pool_len;              /* current length of the stack */
 
 claw_pvar* claw_pool_vget_a(claw_ptr index) {   /* absolute index of the item */
     claw_ptr pool_index = 0;   /* the absolute index within the data pool */
-    claw_size* item_size;
     
-    for(; index > 0; index--) {
-        item_size = (claw_size*)(claw_var_pool + pool_index);
-        pool_index += *item_size + sizeof(claw_size);
-    }
+    for(; index > 0; index--)
+        pool_index += (*((uint16_t *)(claw_var_pool + pool_index))) + sizeof(claw_size);
     
     return ((claw_pvar*) (claw_var_pool + pool_index));
 }
 
 claw_pvar* claw_pool_vget_r(claw_ptr offset) {   /* offset from the last item in the pool */
-    return claw_pool_vget_a(claw_var_pool_len - offset);
+    return claw_pool_vget_a(claw_var_pool_len - offset - 1);
 }
 
 claw_size claw_pool_usage(void) {
-    claw_ptr pool_index = 0;    /* the absolute index within the data pool */
     claw_ptr pool_item = claw_var_pool_len;     /* counter variable */
     claw_size pool_size = 0;     /* total pool size */
-    claw_size* item_size;         /* current item size */
     
-    for(; pool_item > 0; pool_index--) {
-        item_size = (claw_size*)(claw_var_pool + pool_index);
-        pool_index += *item_size + sizeof(claw_size);
-        pool_size += *item_size + sizeof(claw_size);
-    }
+    for(; pool_item > 0; pool_item--)
+        pool_size += (*((uint16_t *)(claw_var_pool + pool_size))) + sizeof(claw_size);
     
     return pool_size;
 }
@@ -41,6 +33,9 @@ claw_size claw_pool_usage(void) {
 uint8_t claw_pool_vcreate(claw_size size) {  /* create pool item with size */
     if(claw_pool_usage() + size < VAR_POOL_SIZE) {
         claw_var_pool_len++;
+        claw_pvar* var;
+        var = claw_pool_vget_r(0);
+        var->size = size;
         return 1;
     }
     
