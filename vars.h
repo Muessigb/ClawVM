a8bb5bc261e3e87a227b12278789dfff9a383594
Benedikt Müssig <benedikt.muessig@gmail.com>
2016-05-07 23:08:32 +0200

    More unit tests and improvements

--- vars.h
+++ vars.h
@@ -7,16 +7,16 @@
 extern uint8_t      claw_pool[];     /* the data pool */
 extern claw_size    claw_pool_len;  /* current length of the stack */
 
-claw_pvar*      claw_pool_vgeta     (claw_ptr index);
-claw_pvar*      claw_pool_vgetr     (claw_ptr offset);
-claw_pvar_s*    claw_pool_vgetsa    (claw_ptr index);
-claw_pvar_s*    claw_pool_vgetsr    (claw_ptr offset);
-claw_pvar_l*    claw_pool_vgetla    (claw_ptr index);
-claw_pvar_l*    claw_pool_vgetlr    (claw_ptr offset);
+claw_pvar*      claw_pool_vget_a    (claw_ptr index);
+claw_pvar*      claw_pool_vget_r    (claw_ptr offset);
+claw_pvar_s*    claw_pool_vget_sa   (claw_ptr index);
+claw_pvar_s*    claw_pool_vget_sr   (claw_ptr offset);
+claw_pvar_l*    claw_pool_vget_la   (claw_ptr index);
+claw_pvar_l*    claw_pool_vget_lr   (claw_ptr offset);
 uint16_t        claw_pool_usage     (void);
-claw_pvar       claw_pool_createv   (claw_size size);
-claw_pvar_s     claw_pool_createv_s (claw_size size);
-claw_pvar_l     claw_pool_createv_l (claw_size size);
+claw_pvar*      claw_pool_vcreate   (claw_size size);
+claw_pvar_s*    claw_pool_vcreate_s (claw_size size);
+claw_pvar_l*    claw_pool_vcreate_l (claw_size size);
 uint8_t         claw_pool_vdestroy  (void);
 
 #endif
