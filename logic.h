4748ffcb98942c0ffaccd06ab77f4aa922cb7723
Benedikt Müssig <benedikt.muessig@gmail.com>
2016-05-08 22:27:45 +0200

    Started with logic

---
+++ logic.h
@@ -0,0 +1,20 @@
+#ifndef LOGIC_H_
+#define LOGIC_H_
+
+#include <stdint.h>
+
+/*claw_error  claw_maths_lnot
+claw_error  claw_maths_land
+claw_error  claw_maths_lor
+claw_error  claw_maths_eq
+claw_error  claw_maths_eq_c
+claw_error  claw_maths_gt
+claw_error  claw_maths_gt_c
+claw_error  claw_maths_gteq
+claw_error  claw_maths_gteq_c
+claw_error  claw_maths_lt
+claw_error  claw_maths_lt_c
+claw_error  claw_maths_lteq
+claw_error  claw_maths_lteq_c*/
+
+#endif
